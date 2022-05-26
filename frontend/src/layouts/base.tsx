6f8e97c4bb2e83b413eb255390eadc606f6383c3
Sudhir Attri <sudhir18267@iiitd.ac.in>
2022-05-26 21:20:30 +0530

    added blur

--- frontend/src/layouts/base.tsx
+++ frontend/src/layouts/base.tsx
@@ -1,27 +1,27 @@
-import React, {ReactNode, useEffect, useState} from "react"
+import React, { ReactNode, useEffect, useState } from "react"
 import { withPrefix } from "gatsby";
 import Helmet from "react-helmet";
 import "../sass/site.scss"
 import { CloudyNightOutline, SunnyOutline } from 'react-ionicons'
 import DarkModeToggle from "react-dark-mode-toggle";
 
-export default function BaseLayout({ children, props}: {children:any,props:any}) {
+export default function BaseLayout({ children, props }: { children: any, props: any }) {
     const [theme, setTheme] = useState("dark");
     useEffect(() => {
         if (typeof window !== 'undefined') {
-        let toggle : HTMLElement = document.getElementById("theme-toggle")!;
-        let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-        if (storedTheme)
-          document.documentElement.setAttribute('data-theme', storedTheme) 
-          setTheme(storedTheme);
+            let toggle: HTMLElement = document.getElementById("theme-toggle")!;
+            let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+            if (storedTheme)
+                document.documentElement.setAttribute('data-theme', storedTheme)
+            setTheme(storedTheme);
         }
         // switchTheme(undefined)
-    },[]);
-    function switchTheme(event: any){
+    }, []);
+    function switchTheme(event: any) {
         if (typeof window !== 'undefined') {
             let currentTheme = document.documentElement.getAttribute("data-theme");
             let targetTheme = "light";
-            
+
             if (currentTheme === "light") {
                 targetTheme = "dark";
             }
@@ -31,42 +31,42 @@ export default function BaseLayout({ children, props}: {children:any,props:any})
         }
     }
     return (
-    <React.Fragment>
-        <Helmet title="foo bar" defer={false} />
-        <nav>
-            <ul>
-                <li><a href="#" ></a></li>
-            </ul>
-            <ul>
-                <li><a href="#" >Blog</a></li>
-                <li><a href="#" >Showcase</a></li>
-                <li><a href="#" >Links</a></li>
-                <li><a href="#" >Resume</a></li>
-            </ul>
-            <ul>
-                {/* <li><a href="#" className="secondary">Resume</a></li> */}
-                <label htmlFor="switch" style={{fontSize:"0.9rem"}}>
-                    {theme == 'dark'? <SunnyOutline cssClasses={"ionic-icon"} color={'#00000'}></SunnyOutline>: <CloudyNightOutline cssClasses={"ionic-icon"} color={'#00000'}></CloudyNightOutline> }
-                    <input onClick={switchTheme} type="checkbox" id="switch" name="switch" role="switch"/>
-                </label>
-                {/* <label id="switch" className="switch">
+        <React.Fragment>
+            <Helmet title="foo bar" defer={false} />
+            <nav>
+                <ul>
+                    <li><a href="#" className="goto">Sudhir Attri</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#" className="goto">Blog</a></li>
+                    <li><a href="#" className="goto goto-active">Showcase</a></li>
+                    <li><a href="#" className="goto">Links</a></li>
+                    <li><a href="#" className="goto">Resume</a></li>
+                </ul>
+                <ul>
+                    {/* <li><a href="#" className="secondary">Resume</a></li> */}
+                    <label htmlFor="switch" style={{ fontSize: "0.9rem" }}>
+                        {theme == 'dark' ? <SunnyOutline cssClasses={"ionic-icon"} color={'#00000'}></SunnyOutline> : <CloudyNightOutline cssClasses={"ionic-icon"} color={'#00000'}></CloudyNightOutline>}
+                        <input checked={theme == "dark" ? (true) : (false)} onClick={switchTheme} type="checkbox" id="switch" name="switch" role="switch" />
+                    </label>
+                    {/* <label id="switch" className="switch">
                     <input type="checkbox" onClick={switchTheme} id="slider"/>
                     <span className="slider round"></span>
                 </label> */}
-                {/* <DarkModeToggle
+                    {/* <DarkModeToggle
                     onChange={switchTheme}
                     size={60}
                 /> */}
-            </ul>
-        </nav>
-        <section className="section">
-            <div className="container">
-                {children}
-            </div>
-        </section>
-        {/* <Helmet>
+                </ul>
+            </nav>
+            <section className="section">
+                <div className="container">
+                    {children}
+                </div>
+            </section>
+            {/* <Helmet>
             <script src={"Script.js"} />    
-        </Helmet> */}
-    </React.Fragment>
+            </Helmet> */}
+        </React.Fragment>
     )
 }
